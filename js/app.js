06937eb57a26d71b0547b76afb2a84860ab9cce8
pktherock <pktherock00@gmail.com>
2023-08-25 01:42:22 +0530

    implement api key security

--- js/app.js
+++ js/app.js
@@ -1,2 +1,11 @@
-const BASE_URL = 'http://developer.marvel.com/v1/public';
-const PUBLIC_KEY = 'fecd62a0015413ebfe8388622dbfd6ac';
+// const BASE_URL = 'http://developer.marvel.com/v1/public';
+// const PUBLIC_KEY = 'fecd62a0015413ebfe8388622dbfd6ac';
+// const PRIVATE_KEY = '2089fdf4c38b36198128768e74523e71';
+
+const BASE_URL = process.env.BASE_URL;
+const PUBLIC_KEY = process.env.PUBLIC_KEY;
+const PRIVATE_KEY = process.env.PRIVATE_KEY;
+
+console.log('Base Url', BASE_URL);
+console.log('Public Key', PUBLIC_KEY);
+console.log('Private Key', PRIVATE_KEY);
